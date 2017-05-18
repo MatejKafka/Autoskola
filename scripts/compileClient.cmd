8cc03f4aee7622ef0df3f490fb9475ca32f00461
Anagmate <anemgare@gmail.com>
2017-05-19 00:40:38 +0200

    updated compileClient script - added "js" dir existence check

--- scripts/compileClient.cmd
+++ scripts/compileClient.cmd
@@ -1,8 +1,11 @@
-::@echo off
+@echo off
 setlocal
 cd %~dp0
 cd ../client/res
 
-browserify -t coffeeify --debug --extension=".coffee" ./coffee/index.coffee > ./js/bundle.js
+if not exist .\js\NUL (
+    mkdir .\js
+)
+call browserify -t coffeeify --debug --extension=".coffee" ./coffee/index.coffee > ./js/bundle.js
 
 endlocal
