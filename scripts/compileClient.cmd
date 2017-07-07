de05b77b4f573d856fe909881319df5dd6bffa78
Anagmate <anemgare@gmail.com>
2017-07-07 15:00:24 +0200

    rewritten updateClient script to use node (works both in Windows and WSL)
    downloaded new images, updated test data

--- scripts/compileClient.cmd
+++ scripts/compileClient.cmd
@@ -1,13 +1,1 @@
-@echo off
-setlocal
-cd %~dp0
-
-if not exist ..\client\res\js\NUL (
-    mkdir ..\client\res\js
-)
-
-call "./node_modules/.bin/browserify.cmd" ^
-    -t coffeeify --debug --extension=".coffee" ^
-     "../client/res/coffee/index.coffee" > "../client/res/js/bundle.js"
-
-endlocal
+node ./compileClient
