4285cb1062cce71e77091065d94439ea732939fa
Matej Kafka <git@a-n.cc>
2020-12-08 01:03:32 +0100

    -

--- phpServer/.php/suppressErrors.php
+++ phpServer/.php/suppressErrors.php
@@ -1,17 +1,20 @@
 <?php
-error_reporting(0);
+# unless DEBUG env var is set, supress all error output
+if (!getenv("DEBUG")) {
+	error_reporting(0);
 
-function errorHandler ($errno = null, $errmsg = null) {
-	echo '500 Internal Server Error';
-	http_response_code(500);
-	exit();
-}
-
-set_error_handler('errorHandler');
-
-register_shutdown_function(function () {
-	$lastErr = error_get_last();
-	if ($lastErr['type'] == E_ERROR) {
-		errorHandler(E_ERROR, $lastErr['message']);
+	function errorHandler ($errno = null, $errmsg = null) {
+		echo '500 Internal Server Error';
+		http_response_code(500);
+		exit();
 	}
-});
+
+	set_error_handler('errorHandler');
+
+	register_shutdown_function(function () {
+		$lastErr = error_get_last();
+		if ($lastErr != NULL && $lastErr['type'] == E_ERROR) {
+			errorHandler(E_ERROR, $lastErr['message']);
+		}
+	});
+}
